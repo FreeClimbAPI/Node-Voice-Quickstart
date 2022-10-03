35952ec5dbf78102e18294e27a18432ef5c5498e
ngarg-vail <ngarg@vailsys.com>
2022-10-03 10:14:52 -0500

    Update tutorial

--- gettingStarted-1.js
+++ gettingStarted-1.js
@@ -1,15 +1,21 @@
 // snippet from server.js or the root-level file of your choice for your app
 const express = require('express')
 const app = express()
-const freeclimbSDK = require('@freeclimb/sdk')
-var port = process.env.PORT || 80
+const { DefaultApi, createConfiguration }= require('@freeclimb/sdk')
+const port = process.env.PORT || 80
 
 // your freeclimb API key (available in the Dashboard) - be sure to set up environment variables to store these values
 const accountId = process.env.ACCOUNT_ID
 const apiKey = process.env.API_KEY
-const freeclimb = freeclimbSDK(accountId, apiKey)
+const configuration = createConfiguration({
+    username: accountId,
+    password: apiKey
+})
+const freeclimb = new DefaultApi(configuration)
 
 // Handles incoming requests on the /incomingCall endpoint
 app.post('/incomingCall', (req, res) => res.status(200))
 
-app.listen(port)
+app.listen(port, () => {
+    console.log('Running on port', port)
+})
--- gettingStarted-2.js
+++ gettingStarted-2.js
@@ -1,20 +1,26 @@
 const express = require('express')
 const app = express()
-const freeclimbSDK = require('@freeclimb/sdk')
-var port = process.env.PORT || 80
+const { DefaultApi, createConfiguration, Say, PerclScript } = require('@freeclimb/sdk')
+const port = process.env.PORT || 80
 
 // your freeclimb API key (available in the Dashboard) - be sure to set up environment variables to store these values
 const accountId = process.env.ACCOUNT_ID
 const apiKey = process.env.API_KEY
-const freeclimb = freeclimbSDK(accountId, apiKey)
+const configuration = createConfiguration({
+    username: accountId,
+    password: apiKey
+})
+const freeclimb = new DefaultApi(configuration)
 
 // Handles incoming requests on the /incomingCall endpoint
 app.post('/incomingCall', (req, res) => {
   // Create Say script to greet caller
-  const hello = freeclimb.percl.say("Hello world!")
+  const hello = new Say({ text: "Hello world!" })
 
   // Add greeting to PerCL script and append to response
-  res.status(200).json(freeclimb.percl.build(hello))
+  res.status(200).json(new PerclScript({ commands: [hello] }).build())
 })
 
-app.listen(port)
+app.listen(port, () => {
+  console.log('Running on port', port)
+})
