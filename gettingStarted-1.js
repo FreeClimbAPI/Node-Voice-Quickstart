4ef9a905aa09ea0554f81130d296c45b06fba2f3
ngarg-vail <ngarg@vailsys.com>
2022-10-03 11:26:41 -0500

    Fix configuration

--- gettingStarted-1.js
+++ gettingStarted-1.js
@@ -1,16 +1,13 @@
 // snippet from server.js or the root-level file of your choice for your app
 const express = require('express')
 const app = express()
-const { DefaultApi, createConfiguration }= require('@freeclimb/sdk')
+const { DefaultApi, createConfiguration } = require('@freeclimb/sdk')
 const port = process.env.PORT || 80
 
 // your freeclimb API key (available in the Dashboard) - be sure to set up environment variables to store these values
 const accountId = process.env.ACCOUNT_ID
 const apiKey = process.env.API_KEY
-const configuration = createConfiguration({
-    username: accountId,
-    password: apiKey
-})
+const configuration = createConfiguration({ accountId, apiKey })
 const freeclimb = new DefaultApi(configuration)
 
 // Handles incoming requests on the /incomingCall endpoint
--- gettingStarted-2.js
+++ gettingStarted-2.js
@@ -6,10 +6,7 @@ const port = process.env.PORT || 80
 // your freeclimb API key (available in the Dashboard) - be sure to set up environment variables to store these values
 const accountId = process.env.ACCOUNT_ID
 const apiKey = process.env.API_KEY
-const configuration = createConfiguration({
-    username: accountId,
-    password: apiKey
-})
+const configuration = createConfiguration({ accountId, apiKey })
 const freeclimb = new DefaultApi(configuration)
 
 // Handles incoming requests on the /incomingCall endpoint
