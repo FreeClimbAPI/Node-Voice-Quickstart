23cee8a961f5800692f9520c5b944d7dfef49685
Kate Brune <k8brune@gmail.com>
2023-04-24 09:40:09 -0500

    update quickstart conventions (#8)
    Co-authored-by: Kate Brune <kbrune@vailsys.com>

--- index.js
+++ index.js
@@ -16,12 +16,6 @@ const freeclimbConfig = freeclimbSDK.createConfiguration({
 });
 const freeclimb = new freeclimbSDK.DefaultApi(freeclimbConfig);
 
-// dynmically retrive hostname information to add to NGROK
-// the purposes of these three lines is informational only and can be removed
-const os = require("os");
-const hostname = os.hostname();
-const yourURL = `http://${hostname}:${port}`;
-
 // Responds to incoming FreeClimb inbound webhook with PerCl
 // https://docs.freeclimb.com/reference/inbound
 app.post("/incomingCall", (req, res) => {
@@ -45,6 +39,8 @@ app.get("/incomingCall", (req, res) => {
 });
 
 app.listen(port, () => {
+  const localUrl = `http://127.0.0.1:${port}`
+
   console.log(`\nWelcome to FreeClimb!\n`);
   if (typeof accountId === "undefined" || typeof apiKey === "undefined") {
     console.log("WARNING! - Your environment variables are not set.");
@@ -52,20 +48,21 @@ app.listen(port, () => {
       "Refer to https://www.npmjs.com/package/dotenv for further instructions.\n"
     );
   } else {
-    console.log(`Your api key is: ${apiKey}`);
-    console.log(`Your account id: ${accountId}\n`);
+    const obfuscatedApiKey = apiKey.replace(/.(?=.{4,}$)/g, '*')
+    console.log(`Your account id: ${accountId}`);
+    console.log(`Your api key is: ${obfuscatedApiKey}\n`);
   }
 
-  console.log(`Your web server is listening at: ${yourURL}`);
+  console.log(`Your web server is listening at: ${localUrl}`);
   console.log(
-    `View an example perCl JSON response to FreeClimb at: ${yourURL}/incomingCall\n`
+    `View an example perCl JSON response to FreeClimb at: ${localUrl}/incomingCall\n`
   );
 
   console.log(
     `Your NEXT STEP is to use NGROK to proxy HTTP Traffic to this local web server.`
   );
   console.log(
-    `  1. In NGROK, configure the dynamic url generate to proxy to ${yourURL}`
+    `  1. In NGROK, configure the dynamic url generate to proxy to ${localUrl}`
   );
   console.log(
     `  2. In the Dashboard or API, set your FreeClimb Application Voice Url to the dynamic NGROK endpoint generated.`
